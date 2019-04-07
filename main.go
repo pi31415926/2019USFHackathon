c5f3e0eea3cb4062b417ee78984034f6f84b1eb2
pi31415926 <st271828@sina.com>
2019-04-06 17:41:51 -0700

    added static file feeding

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"./p3"
-	"./p3/data"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,9 +9,9 @@ import (
 )
 
 func main() {
-	data.TestPeerListRebalance()
 	router := p3.NewRouter()
 	if len(os.Args) == 2 {
+		http.Handle("/", http.FileServer(http.Dir("./frontEnd")))
 		log.Fatal(http.ListenAndServe(":"+os.Args[1], router))
 	} else {
 		fmt.Println("usage: go run main.go <port number>")
